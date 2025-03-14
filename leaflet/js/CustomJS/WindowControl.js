8ddf0fb3392d6cd374a2ab119c2600b9e940e67a
Sean <se2meya@gmail.com>
2025-03-14 16:20:05 -0400

    Update layout and add top link to reference web odm and bush data view

--- leaflet/js/CustomJS/WindowControl.js
+++ leaflet/js/CustomJS/WindowControl.js
@@ -14,10 +14,10 @@ function changeWidth()
 // Function to expand the data view
 function openDataView()
 {
-        document.getElementById('left').style.width = '60%';
-        document.getElementById('right').style.width = '40%';
-        document.getElementById('collapseButton').style.width = '5%';
-        document.getElementById('infoDisp').style.width = '100%';
+        leftPercent = '60%'
+        document.getElementById('left').style.width = leftPercent;
+        document.getElementById('collapseContainer').style.left = leftPercent;
+        document.getElementById('right').style.width = '38%';
         document.getElementById('collapseButton').innerHTML = ">";
         collapsed = false;
 }
@@ -25,10 +25,10 @@ function openDataView()
 // Function to collapse the data view
 function closeDataView()
 {
-        document.getElementById('left').style.width = '98%';
-        document.getElementById('right').style.width = '2%';
-        document.getElementById('collapseButton').style.width = '100%';
-        document.getElementById('infoDisp').style.width = '0%';
+        leftPercent = '98%'
+        document.getElementById('left').style.width = leftPercent;
+        document.getElementById('collapseContainer').style.left = leftPercent;
+        document.getElementById('right').style.width = '0';
         document.getElementById('collapseButton').innerHTML = "<";
         collapsed = true;
 }
